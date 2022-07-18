faaf2d6dedc702f18b39ab41ecb0da620c7518f9
Nick Eberts <nickeberts@google.com>
2022-07-18 14:33:22 -0400

    Fixed cluster remove script

--- gke-fleets-with-argocd/scripts/fleet_cluster_remove.sh
+++ gke-fleets-with-argocd/scripts/fleet_cluster_remove.sh
@@ -4,24 +4,22 @@ set -Eeuo pipefail
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-while getopts p:n:l:c: flag
+while getopts p:n:l: flag
 do
     case "${flag}" in
-        p) PROJECT=${OPTARG};;
+        p) PROJECT_ID=${OPTARG};;
         n) CLUSTER_NAME=${OPTARG};;
         l) CLUSTER_LOCATION=${OPTARG};;
-        c) CONTROL_PLANE_CIDR=${OPTARG};;
     esac
 done
 
 echo "::Variable set::"
-echo "PROJECT: ${PROJECT}"
+echo "PROJECT: ${PROJECT_ID}"
 echo "CLUSTER_NAME: ${CLUSTER_NAME}"
 echo "CLUSTER_LOCATION: ${CLUSTER_LOCATION}"
-echo "CONTROL_PLANE_CIDR:${CONTROL_PLANE_CIDR}"
 
-gcloud container fleet memberships unregister ${CLUSTER_NAME} --gke-cluster=${CLUSTER_LOCATION}/${CLUSTER_NAME} -q
-gcloud container clusters delete ${CLUSTER_NAME} --zone ${CLUSTER_LOCATION} -q
+gcloud container fleet memberships unregister ${CLUSTER_NAME} --gke-cluster=${CLUSTER_LOCATION}/${CLUSTER_NAME} --project ${PROJECT_ID} -q
+gcloud container clusters delete ${CLUSTER_NAME} --zone ${CLUSTER_LOCATION} --project ${PROJECT_ID} -q
 ARGO_CLUSTER_TO_REMOVE=$(argocd cluster list --grpc-web | grep ${CLUSTER_NAME} | awk '{print $1}')
 argocd cluster rm $ARGO_CLUSTER_TO_REMOVE --grpc-web
 
