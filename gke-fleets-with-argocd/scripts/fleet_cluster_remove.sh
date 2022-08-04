f653531b8551e3f3e291e47d0a982c3843acf701
Nick Eberts <nickeberts@google.com>
2022-08-04 11:14:05 -0400

    Fixed bug in cluster remove script

--- gke-fleets-with-argocd/scripts/fleet_cluster_remove.sh
+++ gke-fleets-with-argocd/scripts/fleet_cluster_remove.sh
@@ -21,6 +21,7 @@ echo "CLUSTER_LOCATION: ${CLUSTER_LOCATION}"
 gcloud container fleet memberships unregister ${CLUSTER_NAME} --gke-cluster=${CLUSTER_LOCATION}/${CLUSTER_NAME} --project ${PROJECT_ID} -q
 gcloud container clusters delete ${CLUSTER_NAME} --zone ${CLUSTER_LOCATION} --project ${PROJECT_ID} -q
 # ARGO_CLUSTER_TO_REMOVE=$(argocd cluster list --grpc-web | grep ${CLUSTER_NAME} | awk '{print $1}')
-argocd cluster rm ${CLUSTER_NAME} --grpc-web
+# argocd cluster rm ${CLUSTER_NAME} --grpc-web
+kubectl delete secret ${CLUSTER_NAME} -n argocd --context mccp-central-01
 kctx -d ${CLUSTER_NAME}
 
